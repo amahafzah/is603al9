5f08703501497f60fb5b3087eded761f8c68c2c6
Al Mahafzah <aam34@njit.edu>
2018-07-10 22:51:33 -0400

    This is my entire commit.

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    function store(Request $request) {
+
+        $name = $request->name;
+
+        return redirect()->route('thanks',['name' => $name ]);
+    }
+    function thanks($name, Request $request) {
+
+
+
+        return view('thankyou')->with(compact('name'));
+}
